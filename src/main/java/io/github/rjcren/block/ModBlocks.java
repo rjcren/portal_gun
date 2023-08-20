09d81846c5c3dba0f76549f3f7f18b2eba06ed00
2747571193 <2747571193@qq.com>
2023-08-20 20:47:38 +0800

    添加传送块

--- src/main/java/io/github/rjcren/block/ModBlocks.java
+++ src/main/java/io/github/rjcren/block/ModBlocks.java
@@ -2,15 +2,20 @@ package io.github.rjcren.block;
 
 import io.github.rjcren.PortalGunMod;
 import io.github.rjcren.item.ModItems;
+import net.minecraft.world.Nameable;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
 
 /**
  * ClassName: ModBlocks
@@ -28,8 +33,18 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, PortalGunMod.MODID);
 
+    public static final RegistryObject<Block> PORTAL_BLOCK = registryBlock("portal_block_1",
+            () -> new Block(BlockBehaviour.Properties.of(Material.PORTAL).strength(0)), CreativeModeTab.TAB_MISC);
+
     //
-    private static <T extends Block>RegistryObject<Item> registryBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+    private static <T extends Block> RegistryObject<T> registryBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registryBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    //
+    private static <T extends Block> RegistryObject<Item> registryBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
 
